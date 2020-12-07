678769d35182a1d63f86219c05a4aa6137bc1ce6
bpcastilho <brunopcastilho@gmail.com>
2020-12-07 16:45:38 -0300

    repository db

--- dockerfile
+++ dockerfile
@@ -1,5 +1,4 @@
 FROM openjdk:8-jdk-alpine
-VOLUME /tmp
-ARG JAR_FILE
-ADD ${JAR_FILE} landingpage-0.0.1-SNAPSHOT.jar
-ENTRYPOINT ["java", "-Djava.security.ecd=file:/dev/./urandom", "-jar", "/landingpage-0.0.1-SNAPSHOT.jar"]
+ARG JAR_FILE=target/landingpage-1.0.1.jar
+COPY ${JAR_FILE} landingpage-1.0.1.jar
+ENTRYPOINT ["java","-jar","/landingpage-1.0.1.jar"]
--- src/main/java/com/bpcastilho/shopapp/landingpage/repository/LandingPageRepository.java
+++ src/main/java/com/bpcastilho/shopapp/landingpage/repository/LandingPageRepository.java
@@ -2,12 +2,20 @@ package com.bpcastilho.shopapp.landingpage.repository;
 
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.json.GsonJsonParser;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.core.PreparedStatementCreatorFactory;
+import org.springframework.jdbc.core.SqlParameter;
 import org.springframework.stereotype.Repository;
 
 import com.bpcastilho.shopapp.representation.ClientRepresentation;
 
 import lombok.extern.slf4j.Slf4j;
+
+import java.sql.PreparedStatement;
+import java.util.ArrayList;
+
 import org.slf4j.Logger;
 
 @Repository
@@ -23,9 +31,19 @@ public class LandingPageRepository {
 	}
 	
 	public void addClientLandingPage(ClientRepresentation body) {
-		//template.
 		
-		logger.info("Repository");
+
+		logger.info("repository - " + body.toString());
+		try {
+		template.update("insert into shopappdb.Client \r\n"
+				+ "(name, ip, email)\r\n"
+				+ "values\r\n"
+				+ "(?,?,?)", body.getName(), body.getIp(), body.getEmail());
+		} catch (Exception ex) {
+			logger.error(ex.toString());
+		}
+		
+		
 		
 		
 		
